1fc32cf0baa54a7fc5dcbd89691a1d4dcbede90d
Umair Mirza <umair_hp@hotmail.com>
2022-06-13 21:10:40 +0500

    fixed layout

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -9,9 +9,9 @@ function UserResults() {
   if (!loading) {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-          {users.map((user) => (
-            <UserItem key={user.id} user={user} />
-          ))}
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
       </div>
     )
   } else {
